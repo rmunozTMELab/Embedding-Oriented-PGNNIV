6d68e5d05d08e862a0bce589562b4f97b49972dc
rmunozTMELab <739163@unizar.es>
2025-07-31 11:19:37 +0200

    Deleting some deprecated code

--- PGNNIV/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package for generating various datasets.
-"""
-from .linear_homogeneous import data_generator
-from .non_linear import data_generator
-from .non_linear_sigmoid import data_generator
-
-
--- PGNNIV/datasets/linear_homogeneous/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Module for the linear homogeneous dataset.
-"""
-# from .data_generator import generate_data
-# from .config import DATASET_CONFIG
